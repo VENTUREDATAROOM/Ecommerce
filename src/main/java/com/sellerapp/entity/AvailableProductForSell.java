dfbe83df0f09b491fc77bd39ae881b6017e1bd6b
GORANK <GORANK@192.168.71.1>
2024-03-13 16:26:21 +0530

    commit changes for prodcut for sell and order for it

--- src/main/java/com/sellerapp/entity/AvailableProductForSell.java
+++ src/main/java/com/sellerapp/entity/AvailableProductForSell.java
@@ -5,11 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+
+
 @Entity
 public class AvailableProductForSell {
 
@@ -33,5 +31,153 @@ public class AvailableProductForSell {
 	private String orderCode;
 	private String harvestDate;
 	private String transactionId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductSubName() {
+		return productSubName;
+	}
+	public void setProductSubName(String productSubName) {
+		this.productSubName = productSubName;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getProductMasterCode() {
+		return productMasterCode;
+	}
+	public void setProductMasterCode(String productMasterCode) {
+		this.productMasterCode = productMasterCode;
+	}
+	public String getProductMasterSubCode() {
+		return ProductMasterSubCode;
+	}
+	public void setProductMasterSubCode(String productMasterSubCode) {
+		ProductMasterSubCode = productMasterSubCode;
+	}
+	public String getMandiName() {
+		return mandiName;
+	}
+	public void setMandiName(String mandiName) {
+		this.mandiName = mandiName;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getExpectedPrice() {
+		return expectedPrice;
+	}
+	public void setExpectedPrice(String expectedPrice) {
+		this.expectedPrice = expectedPrice;
+	}
+	public String getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(String totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getQuality() {
+		return quality;
+	}
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+	public String getDistanceFromMandi() {
+		return distanceFromMandi;
+	}
+	public void setDistanceFromMandi(String distanceFromMandi) {
+		this.distanceFromMandi = distanceFromMandi;
+	}
+	public String getOrderOtp() {
+		return orderOtp;
+	}
+	public void setOrderOtp(String orderOtp) {
+		this.orderOtp = orderOtp;
+	}
+	public String getOrderCode() {
+		return orderCode;
+	}
+	public void setOrderCode(String orderCode) {
+		this.orderCode = orderCode;
+	}
+	public String getHarvestDate() {
+		return harvestDate;
+	}
+	public void setHarvestDate(String harvestDate) {
+		this.harvestDate = harvestDate;
+	}
+	public String getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+	@Override
+	public String toString() {
+		return "AvailableProductForSell [id=" + id + ", productName=" + productName + ", productSubName="
+				+ productSubName + ", userCode=" + userCode + ", productMasterCode=" + productMasterCode
+				+ ", ProductMasterSubCode=" + ProductMasterSubCode + ", mandiName=" + mandiName + ", quantity="
+				+ quantity + ", address=" + address + ", expectedPrice=" + expectedPrice + ", totalPrice=" + totalPrice
+				+ ", location=" + location + ", quality=" + quality + ", distanceFromMandi=" + distanceFromMandi
+				+ ", orderOtp=" + orderOtp + ", orderCode=" + orderCode + ", harvestDate=" + harvestDate
+				+ ", transactionId=" + transactionId + "]";
+	}
+	public AvailableProductForSell(Long id, String productName, String productSubName, String userCode,
+			String productMasterCode, String productMasterSubCode, String mandiName, String quantity, String address,
+			String expectedPrice, String totalPrice, String location, String quality, String distanceFromMandi,
+			String orderOtp, String orderCode, String harvestDate, String transactionId) {
+		super();
+		this.id = id;
+		this.productName = productName;
+		this.productSubName = productSubName;
+		this.userCode = userCode;
+		this.productMasterCode = productMasterCode;
+		ProductMasterSubCode = productMasterSubCode;
+		this.mandiName = mandiName;
+		this.quantity = quantity;
+		this.address = address;
+		this.expectedPrice = expectedPrice;
+		this.totalPrice = totalPrice;
+		this.location = location;
+		this.quality = quality;
+		this.distanceFromMandi = distanceFromMandi;
+		this.orderOtp = orderOtp;
+		this.orderCode = orderCode;
+		this.harvestDate = harvestDate;
+		this.transactionId = transactionId;
+	}
+	public AvailableProductForSell() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
--- src/main/java/com/sellerapp/model/AvailableProductRequest.java
+++ src/main/java/com/sellerapp/model/AvailableProductRequest.java
@@ -1,14 +1,7 @@
 package com.sellerapp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+
 public class AvailableProductRequest {
 
 	private String productName;
@@ -19,5 +12,77 @@ public class AvailableProductRequest {
 	private String address;
 	private String distanceFromMandi;
 	private String harvestDate;
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getMandiName() {
+		return mandiName;
+	}
+	public void setMandiName(String mandiName) {
+		this.mandiName = mandiName;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getQuality() {
+		return quality;
+	}
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDistanceFromMandi() {
+		return distanceFromMandi;
+	}
+	public void setDistanceFromMandi(String distanceFromMandi) {
+		this.distanceFromMandi = distanceFromMandi;
+	}
+	public String getHarvestDate() {
+		return harvestDate;
+	}
+	public void setHarvestDate(String harvestDate) {
+		this.harvestDate = harvestDate;
+	}
+	@Override
+	public String toString() {
+		return "AvailableProductRequest [productName=" + productName + ", mandiName=" + mandiName + ", quantity="
+				+ quantity + ", quality=" + quality + ", price=" + price + ", address=" + address
+				+ ", distanceFromMandi=" + distanceFromMandi + ", harvestDate=" + harvestDate + "]";
+	}
+	public AvailableProductRequest(String productName, String mandiName, String quantity, String quality, String price,
+			String address, String distanceFromMandi, String harvestDate) {
+		super();
+		this.productName = productName;
+		this.mandiName = mandiName;
+		this.quantity = quantity;
+		this.quality = quality;
+		this.price = price;
+		this.address = address;
+		this.distanceFromMandi = distanceFromMandi;
+		this.harvestDate = harvestDate;
+	}
+	public AvailableProductRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
